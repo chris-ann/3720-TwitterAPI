24ae89095491603c52bd6e10acacdba4d9e320a4
Christine Clark <chrstn.clark@gmail.com>
2018-03-23 13:06:16 -0600

    anonymized settings

--- get_tweets.php
+++ get_tweets.php
@@ -4,15 +4,15 @@
 	 
 	// Set access tokens: https://dev.twitter.com/apps/
 	$settings = array(
-	    'oauth_access_token' => "34574736-8o8OXv0kIdy6DuVKGjq9nqwI9ygxxpNQNsJCgBgnv",
-	    'oauth_access_token_secret' => "oYdm0mGoPOH8P4LxZcWjp4p5By33hoSD9YekpYfVOhtU9",
-	    'consumer_key' => "xC4i6RWvsMVq2I4SVK9HZ9TtR",
-	    'consumer_secret' => "9PYAhhaWETYNdlX9GpIEMP1YNij8y1cCOtk03JQPEkku1jGgud"
+	    'oauth_access_token' => "YOUR_TOKEN",
+	    'oauth_access_token_secret' => "YOUR_TOKEN_SECRET",
+	    'consumer_key' => "YOUR_KEY",
+	    'consumer_secret' => "YOUR_SECRET"
 	);
 
 	// Choose URL and Request Method
 	$url = "https://api.twitter.com/1.1/statuses/mentions_timeline.json";
-	$getfield = '?screen_name=chrisannclark&count=20';
+	$getfield = '?screen_name=chrisannclark&count=20'; // queries start with ? and are strung together with &
 	$requestMethod = "GET";
 	
 	//Perform the request!
